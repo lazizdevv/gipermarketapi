a7d987593ffc3699e81770842d834b1207c8939b
Lazizbek <oddiybolakay05@gmail.com>
2024-08-27 21:55:40 +0500

    end-cors

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-// CORS sozlamalarini kengaytiring
 server.use(cors({
   origin: '*', // Hamma domenlardan so'rovlarni qabul qilish
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: 'Content-Type, Authorization'
 }));
 
 server.use(middlewares);
 server.use(router);
-server.listen(process.env.PORT || 3000, () => {
-  console.log('JSON Server is running');
+
+const port = process.env.PORT || 4173; // 4173 portini sinab ko'ring
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
 });
